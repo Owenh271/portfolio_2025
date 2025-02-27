544aa3953b95bfece7804bcc930256eb962ab17e
maximchihai <maxchacaron77@gmail.com>
2025-02-26 20:00:44 -0800

    lm

--- game/racinggame.js
+++ game/racinggame.js
@@ -1,2 +1,19 @@
 var car,carImg
-var path,pathImg
+var background,backgroundImg
+
+
+function preload(){
+    carImg = loadImage("car.png");
+    backgroundImg = loadImage("background.png");
+}
+
+function setup(){
+    createCanvas(windowWidth,windowHeight);
+    background = createSprite(500,500,50,50);
+    background.addImage("background",backgroundImg);
+} 
+
+function draw(){
+    
+    drawSprites();
+}
